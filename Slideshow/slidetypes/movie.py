63aef6dadbb1b9e29bb4947d18394ac7295f84cc
HansiHE <hansihe@hansihe.com>
2013-06-24 16:31:50 +0200

    tried shell=True

--- Slideshow/slidetypes/movie.py
+++ Slideshow/slidetypes/movie.py
@@ -13,7 +13,7 @@ class MovieSlide(Slide):
     def init(self):
         self.screen.fill((0, 0, 255))
         self.flip_display()
-        self.player = subprocess.Popen(['omxplayer', self.global_config['media_dir'] + self.config['file']], stdin=subprocess.PIPE)
+        self.player = subprocess.Popen(['omxplayer', self.global_config['media_dir'] + self.config['file']], stdin=subprocess.PIPE, shell=True)
         print "rendering %s" % str(self.config)
 
     def tick(self, time):
