db61e44a9d74bbfae376556f3dd63ac8b7b4fde6
HansiHE <hansihe@hansihe.com>
2013-06-25 17:25:38 +0200

    rest api can now start and stop the slideshow

--- Backend-Daemon/slideshow_control.py
+++ Backend-Daemon/slideshow_control.py
@@ -1,22 +1,57 @@
 __author__ = 'HansiHE'
 
 from twisted.web import resource
-from slideshow_manager import SlideshowManager
+from slideshow_manager import SlideshowManager, RestartFailedException, NotRunningException, AlreadyRunningException
+import json
 
 
 slideshow_manager = SlideshowManager()
+
 
 class SlideshowControl(resource.Resource):
     isLeaf = False
 
 slideshow_control = SlideshowControl()
 
+
+class SlideshowStart(resource.Resource):
+    isLeaf = True
+
+    def render_POST(self, request):
+        try:
+            slideshow_manager.start_slideshow()
+        except Exception, e:
+            return json.dumps({'success': False, 'exception': str(e)})
+        return json.dumps({'success': True})
+
+slideshow_start = SlideshowStart()
+
+
+class SlideshowStop(resource.Resource):
+    isLeaf = True
+
+    def render_POST(self, request):
+        try:
+            slideshow_manager.stop_slideshow()
+        except Exception, e:
+            return json.dumps({'success': False, 'exception': str(e)})
+        return json.dumps({'success': True})
+
+slideshow_stop = SlideshowStop()
+
+
 class SlideshowRestart(resource.Resource):
     isLeaf = True
 
     def render_POST(self, request):
-        return 'ye'
+        try:
+            slideshow_manager.restart_slideshow()
+        except Exception, e:
+            return json.dumps({'success': False, 'exception': str(e)})
+        return json.dumps({'success': True})
 
 slideshow_restart = SlideshowRestart()
 
+slideshow_control.putChild('start', slideshow_start)
+slideshow_control.putChild('stop', slideshow_stop)
 slideshow_control.putChild('restart', slideshow_restart)
--- Backend-Daemon/slideshow_manager.py
+++ Backend-Daemon/slideshow_manager.py
@@ -1,12 +1,43 @@
 __author__ = 'HansiHE'
 
 import subprocess
+import signal
 from config import slideshow_path
+
+
+class AlreadyRunningException(Exception):
+    pass
+
+
+class NotRunningException(Exception):
+    pass
+
+
+class RestartFailedException(Exception):
+    pass
 
 
 class SlideshowManager(object):
 
     slideshow_process = None
 
-    def __init__(self):
+    def start_slideshow(self):
+        if self.slideshow_process:
+            raise AlreadyRunningException()
         self.slideshow_process = subprocess.Popen(['sudo', 'python', slideshow_path])
+
+    def stop_slideshow(self):
+        if not self.slideshow_process:
+            raise NotRunningException()
+        self.slideshow_process.send_signal(signal.CTRL_C_EVENT)
+
+    def restart_slideshow(self):
+        try:
+            self.stop_slideshow()
+        except NotRunningException:
+            pass
+
+        try:
+            self.start_slideshow()
+        except AlreadyRunningException:
+            raise RestartFailedException()
